914cec1c46b042af0e569e43c9cad523069ba5b0
Sherif Jimoh <Sherefjimoh@gmail.com>
2024-04-11 21:48:35 +0100

    rendered hero-section ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
 import { Fragment } from 'react';
 
+import Hero from '@/components/Hero';
+import Camp from '@/components/Camp';
+import Guide from '@/components/Guide';
+import Features from '@/components/Features';
+import GetApp from '@/components/GetApp';
+
 export default function Home() {
-  return <Fragment></Fragment>;
+  return (
+    <Fragment>
+      <Hero />
+      <Camp />
+      <Guide />
+      <Features />
+      <GetApp />
+    </Fragment>
+  );
 }
--- components/Button.tsx
+++ components/Button.tsx
@@ -11,9 +11,7 @@ const Button = ({ type, icon, variant, title }: ButtonProps) => {
   return (
     <button
       type={type}
-      className={`flexCenter gap-3 rounded-full border ${
-        title === 'Log in' ? `${variant}` : ''
-      }`} // dynamically change background-color
+      className={`flexCenter gap-3 rounded-full border ${variant}`} // dynamically change background-color
     >
       {icon && <Image src={icon} alt='Log in icon' width={24} height={24} />}
       <label className='bold-16 whitespace-nowrap'>{title}</label>
