b3f5327c16cffd006079bfd8fca23f966d93cc5d
Sherif Jimoh <Sherefjimoh@gmail.com>
2024-04-15 12:09:45 +0100

    rendered get-app ui section

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,16 +5,21 @@ type ButtonProps = {
   icon?: string;
   variant: string;
   title: string;
+  full?: boolean;
 };
 
-const Button = ({ type, icon, variant, title }: ButtonProps) => {
+const Button = ({ type, icon, variant, title, full }: ButtonProps) => {
   return (
     <button
       type={type}
-      className={`flexCenter gap-3 rounded-full border ${variant}`} // dynamically change background-color
+      className={`flexCenter gap-3 rounded-full border ${variant} ${
+        full && 'w-full'
+      }`} // dynamically change background-color
     >
       {icon && <Image src={icon} alt='Log in icon' width={24} height={24} />}
-      <label className='bold-16 whitespace-nowrap'>{title}</label>
+      <label className='bold-16 whitespace-nowrap cursor-pointer'>
+        {title}
+      </label>
     </button>
   );
 };
