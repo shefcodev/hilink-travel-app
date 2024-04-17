661e55daba5cd4931bc20f1165d6eff5a9a8c72d
Sherif Jimoh <Sherefjimoh@gmail.com>
2024-04-17 12:51:30 +0100

    added mobile dropdown-menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,18 @@
+'use client';
+
+import { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { NAV_LINKS } from '@/constants';
 import Button from '@/components/Button';
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  const menuToggleHandler = (event: React.MouseEvent) => {
+    setToggleMenu((prevValue) => !prevValue);
+  };
+
   return (
     <nav className='flexBetween max-container padding-container relative z-30 py-5'>
       <Link href='/'>
@@ -42,7 +51,35 @@ const Navbar = () => {
         width={32}
         height={32}
         className='inline-block cursor-pointer lg:hidden'
+        onClick={menuToggleHandler}
       />
+
+      {toggleMenu && (
+        <div className='hidden absolute z-10 right-6 top-5 max-lg:flex flex-row items-start gap-3 py-3 px-5 bg-white shadow-md transition-all'>
+          <div className='flex-1 flex flex-col gap-2'>
+            {NAV_LINKS.map(({ href, key, label }) => (
+              <Link
+                key={key}
+                href={href}
+                className='regular-14 text-gray-50 cursor-pointer pb-1.5 transition-all hover:font-bold hover:text-green-50 flex justify-start'
+                onClick={menuToggleHandler}
+              >
+                {label}
+              </Link>
+            ))}
+          </div>
+          <div className=''>
+            <Image
+              src='/close.svg'
+              alt='close menu'
+              width={24}
+              height={24}
+              className='invert'
+              onClick={menuToggleHandler}
+            />
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
